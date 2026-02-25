795767599c594c93641efa1cdd2f2e76a417695f
Fedoseev Matvei <165332361+Depak00@users.noreply.github.com>
2026-02-25 23:24:52 +0300

    Delete projects_2 directory

--- projects_2/task_2-3_recipe.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python
-name = input("Введите название питательной среды: ")
-conc = int(input("Введите концентрацию агара (%): "))
-temp = int(input("Введите температуру стерилизации (°C): "))
-with open("recipe.txt", "w", encoding="utf-8") as recipe:
-    recipe.write(f"{name}\n{conc}\t {temp}\n")
-print("Файл 'recipe.txt' успешно сформирован!")
--- projects_2/task_2-3_sensor.py
+++
@@ -1,6 +0,0 @@
-#!/bin/python
-name = input("Введите имя оператора: ")
-pres = int(input("Введите текущее значение давления (Па): "))
-with open("sensor_log.txt", "a", encoding="utf-8") as sens:
-    sens.write(f"{name}\t{pres}\n")
-print("Данные успешно сохранены в sensor_log.txt")
